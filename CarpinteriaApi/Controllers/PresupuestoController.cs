2740e7a95642c2ce7b353be92cac1898b2fd2369
alumno <alumno@GI.FRC.UTN.EDU.AR>
2023-10-24 16:49:42 -0300

    API Carpinteria

---
+++ CarpinteriaApi/Controllers/PresupuestoController.cs
@@ -0,0 +1,51 @@
+﻿using CarpinteriaBack.Entidades;
+using CarpinteriaBack.Fachada.Implementacion;
+using CarpinteriaBack.Fachada.Interfaz;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace CarpinteriaApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PresupuestoController : ControllerBase
+    {
+        private IAplicacion app;
+        public PresupuestoController()
+        {
+            app = new Aplicacion();
+        }
+        [HttpGet("/productos")]
+        public IActionResult GetProductos()
+        {
+            List<Producto> lst;
+            try
+            {
+                lst = app.GetProductos();
+                return Ok(lst);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Error interno!! Intente luego!!");
+            }
+        }
+
+        [HttpPost("/presupuesto")]
+        public IActionResult PostPresupuesto(Presupuesto oPre)
+        {
+            try
+            {
+                if(oPre == null)
+                {
+                    return BadRequest("Presupuesto incorrecto!!");
+                }
+                return Ok(app.SavePresupuesto(oPre));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Error interno!! Intente luego!!");
+            }
+        }
+    }
+}
